0006aedffef6619325c03aa80f3a51f9a81e4dc7
antunys <antunysssmilmo321@gmail.com>
2021-09-13 17:00:59 -0300

    footer e header

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,7 +1,26 @@
+import '../footer/style.scss'
+
+
 export default function Footer() {
     return (
         <>
-            <h1>footer</h1>
+            <div className='footer-container'>
+
+                
+                <div className='menu-footer'>
+                    <div className='logo-cbcenf-mobile'></div>
+                    <a href="#">CONGRESSO BRASILEIRO DOS CONSELHOS DE ENFERMAGEM </a>
+                </div>
+                <div className='logo-cbcenf-footer'> </div>
+
+                <div className='logos-baixo-footer'>
+                    <div className='logo-cofenn-footer'> </div>
+                    <div className='logo-cofen-footer'> </div>
+                </div>
+
+
+            </div>
+            <div className='rodape'></div>
         </>
 
     );
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,24 @@
+import '../header/style.scss'
+
+
 export default function Header() {
     return (
         <>
-            <h1>Header</h1>
+            <div className='header-container'>
+                <div className='logo-cbcenf-header'> </div>
+                <div className='menu-header'>
+                    <ul>
+                        <li> <a href="#">inicio</a>  </li>
+                        <li> <a href="#">Sobre</a>  </li>
+                        <li><a href="#">Programação</a>  </li>
+                        <li><a href="#">Noticias</a>  </li>
+                        <li><a href="#">Redes Sociais</a> </li>
+                    </ul>
+                </div>
+                <div className='logo-cofen-header'>
+
+                </div>
+            </div>
         </>
 
     );
