dd9929ddae8eacd61536952e78288f34469992dd
Bruno Lucas <brunnnolucas@gmail.com>
2021-09-17 17:53:17 -0300

    funcao do componente programacao

--- src/components/programacao/index.tsx
+++ src/components/programacao/index.tsx
@@ -1,36 +1,65 @@
 import '../programacao/style.scss'
-
+import { useState } from 'react';
 export default function Programacao() {
+    let [prog, setProg] = useState(1)
     return (
         <>
-            <div className='container'>
+            <div className='container p-4 md:p-0'>
                 <p className='mini-title text-sm'>ACOMPANHE</p>
                 <h1 className='title-programacao text-5xl'><b>Programação</b></h1>
                 <div className='box-date'>
                     <ul className='flex justify-around color- border-b my-4 h-24'>
-                        <li >
-                            <button className='bg-gray-200 text-center w-64 rounded-2xl h-16 text-3xl'>
-                                <b>dia 12</b>
+                        <li className={`  ${prog === 1 ? 'ative-border' : ''}`}>
+                            <button className={`${prog === 1 ? 'bg-white text-center w-20 md:w-64 rounded-2xl h-16 md:text-3xl text-2xl' : 'bg-gray-200 text-center w-20 md:w-64 rounded-2xl h-16 md:text-3xl text-2xl'} `} onClick={() => setProg(1)} >
+                                <div className="md:flex justify-center">
+                                    <p className="md:mr-2"><b> dia</b> </p>
+                                    <b>12</b>
+                                </div>
                             </button>
                         </li>
-                        <li >
-                            <button className='bg-gray-200 text-center w-64 rounded-2xl h-16 text-3xl'>
-                                <b>dia 13</b>
+                        <li className={`  ${prog === 2 ? 'ative-border' : ''}`}>
+                            <button className={`${prog === 2 ? ' bg-white text-center w-20 md:w-64 rounded-2xl h-16 md:text-3xl text-2xl' : 'bg-gray-200 text-center w-20 md:w-64 rounded-2xl h-16 md:text-3xl text-2xl'} `} onClick={() => setProg(2)}>
+                                <div className="md:flex justify-center">
+                                    <p className="md:mr-2"><b> dia</b> </p>
+                                    <b>12</b>
+                                </div>
                             </button>
                         </li>
-                        <li >
-                            <button className='bg-gray-200 text-center w-64 rounded-2xl h-16 text-3xl'>
-                                <b>dia 14</b>
+                        <li className={`  ${prog === 3 ? 'ative-border' : ''}`}>
+                            <button className={`${prog === 3 ? ' bg-white text-center w-20 md:w-64 rounded-2xl h-16 md:text-3xl text-2xl' : 'bg-gray-200 text-center w-20 md:w-64 rounded-2xl h-16 md:text-3xl text-2xl'} `} onClick={() => setProg(3)}>
+                                <div className="md:flex justify-center">
+                                    <p className="md:mr-2"><b> dia</b> </p>
+                                    <b>12</b>
+                                </div>
                             </button>
                         </li>
-                        <li >
-                            <button className='bg-gray-200 text-center w-64 rounded-2xl h-16 text-3xl'>
-                                <b>dia 15</b>
+                        <li className={`  ${prog === 4 ? 'ative-border' : ''}`}>
+                            <button className={`${prog === 4 ? ' bg-white text-center w-20 md:w-64 rounded-2xl h-16 md:text-3xl text-2xl' : 'bg-gray-200 text-center w-20 md:w-64 rounded-2xl h-16 md:text-3xl text-2xl'} `} onClick={() => setProg(4)}>
+                                <div className="md:flex justify-center">
+                                    <p className="md:mr-2"><b> dia</b> </p>
+                                    <b>12</b>
+                                </div>
                             </button>
                         </li>
 
                     </ul>
-                    <div>
+                    <div className={`  ${prog === 1 ? 'block' : 'hidden'}`}>
+                        <h1>Campo 1</h1>
+                        <p>alWIRHGLIAQE</p>
+                        <p>alWIRHGLIAQE</p>
+                    </div>
+                    <div className={`  ${prog === 2 ? 'block' : 'hidden'}`}>
+                        <h1>Campo 2</h1>
+                        <p>alWIRHGLIAQE</p>
+                        <p>alWIRHGLIAQE</p>
+                    </div>
+                    <div className={`  ${prog === 3 ? 'block' : 'hidden'}`}>
+                        <h1>Campo 3</h1>
+                        <p>alWIRHGLIAQE</p>
+                        <p>alWIRHGLIAQE</p>
+                    </div>
+                    <div className={`  ${prog === 4 ? 'block' : 'hidden'}`}>
+                        <h1>Campo 4</h1>
                         <p>alWIRHGLIAQE</p>
                         <p>alWIRHGLIAQE</p>
                     </div>
