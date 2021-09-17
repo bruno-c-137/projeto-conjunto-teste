77d81a2b027c053c2d9fd1d173c57a76880f082d
antunys <antunysssmilmo321@gmail.com>
2021-09-17 08:52:28 -0300

    ajuste footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,21 +1,28 @@
 import '../footer/style.scss'
-
+import cbcenf from '../../assets/images/cbcenf.png'
+import cofen from '../../assets/images/Frame-footer.png'
 
 export default function Footer() {
     return (
         <>
             <div className='footer-container'>
 
-                
+
                 <div className='menu-footer'>
                     <div className='logo-cbcenf-mobile'></div>
                     <a href="#">CONGRESSO BRASILEIRO DOS CONSELHOS DE ENFERMAGEM </a>
                 </div>
-                <div className='logo-cbcenf-footer'> </div>
+                <div className='logo-cbcenf-footer'>
+                    <img src={cbcenf} alt="" />
+                </div>
 
                 <div className='logos-baixo-footer'>
-                    <div className='logo-cofenn-footer'> </div>
-                    <div className='logo-cofen-footer'> </div>
+                    <div className='logo-cofenn-footer mx-4'>
+                        <img src={cofen} alt="" />
+                    </div>
+                    <div className='logo-cofen-footer'>
+                        <img src={cofen} alt="" />
+                    </div>
                 </div>
 
 
