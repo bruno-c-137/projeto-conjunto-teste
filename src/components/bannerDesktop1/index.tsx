684e0b1d33c029df1b6fee419134ee6c4ee1ad6d
Bruno Lucas <brunnnolucas@gmail.com>
2021-09-17 16:24:48 -0300

    adcionando botao

--- src/components/bannerDesktop1/index.tsx
+++ src/components/bannerDesktop1/index.tsx
@@ -10,8 +10,12 @@ export default function BannerDesktop1() {
                         <div className="img1  w-full  md:absolute height-banner3   top-0"></div></div>
                     <div className="container md:relative  p-4 md:p-0">
                         <p className="banner-title text-xl md:text-2xl">#ConexãoCBCENF</p>
-                        <p className="banner-text text-2xl md:text-4xl lg:w-2/5">Juntos no maior evento da Enfermagem na América Latina</p>
+                        <p className="banner-text text-2xl md:text-4xl md:w-2/5">Juntos no maior evento da Enfermagem na América Latina</p>
+                        <div className="mt-3">
+                            <a className="button-header" href="#">INSCREVA-SE</a>
+                        </div>
                     </div>
+
                 </div>
             </div>
 
